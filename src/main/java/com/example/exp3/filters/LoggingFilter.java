43a05f11c7eb521d7380c513c275ce3b140f2caf
小新 <782446723@qq.com>
2023-11-14 16:33:16 +0800

    去除冗余项。

--- src/main/java/com/example/exp3/filters/LoggingFilter.java
+++
@@ -1,4 +0,0 @@
-package com.example.exp3.filters;
-
-public class LoggingFilter {
-}
--- src/main/webapp/views/books/js/book.js
+++
@@ -1,9 +0,0 @@
-function changePageSize(val) {
-    window.location.href = "/exp3/BookServlet?type=list&pageSize=" + val;
-}
-
-function goPageNum(val) {
-    //获取id为goPageNum的input里面的数值
-    const num = document.getElementById("goPageNum").value;
-    window.location.href = "/exp3/BookServlet?type=list&pageSize=" + val + "&pageNum=" + num;
-}
