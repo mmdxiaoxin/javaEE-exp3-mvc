f4015866d21624ecc7e398a32e56c598f171dfe6
小新 <782446723@qq.com>
2024-01-19 17:55:54 +0800

    feat:实习登陆验证服务。

--- src/main/java/com/example/exp3/service/UsersService.java
+++ src/main/java/com/example/exp3/service/UsersService.java
@@ -1,7 +1,11 @@
 package com.example.exp3.service;
 
+import com.example.exp3.daos.impl.UserDAOImpl;
+
 public class UsersService {
+    private final UserDAOImpl userDAO = new UserDAOImpl();
+
     public boolean validateUser(String username, String password) {
-        return true;
+        return userDAO.login(username, password) > 0;
     }
 }
